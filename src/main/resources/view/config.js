d4ab4a6fc729b980f82757deae5c275a91a16c6d
Samuel Gaborieau <sam.gabor@hotmail.com>
2021-07-29 21:02:56 +0200

    initialize blank solo game project;

--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -2,5 +2,9 @@ import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
 
 // List of viewer modules that you want to use in your game
 export const modules = [
+	
+	// Native graphic module API
 	GraphicEntityModule
 ];
+
+export const gameName = "Space Invaders";
---
+++ src/test/java/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+
+		// read entries before game loop
+		// later should read ennemies initial count
+
+		while(true) {
+			// read entries for each game turn
+			// later should read position x y for each ennemy
+			// later should read position x y of player
+			
+			// give output solution
+			System.out.println("Unicorn <3");
+		}
+	}
+}
